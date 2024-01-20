fa0332eaf1403df1b14f74cc1ed4ecbc6ed75ed1
seohyun-lee <applemint2003@gmail.com>
2024-01-20 17:03:48 +0900

    스프링 프로젝트 생성

---
+++ withmate/src/main/java/com/gdscewha/withmate/WithMateApplication.java
@@ -0,0 +1,13 @@
+package com.gdscewha.withmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithMateApplication.class, args);
+	}
+
+}
---
+++ withmate/src/test/java/com/gdscewha/withmate/WithMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscewha.withmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

214968a5f9ba81ec58e1e5239bcdbeb99adeeb49
Dongseon Yang <78548833+dongseon0@users.noreply.github.com>
2024-01-25 14:00:25 +0900

    feat(MatchingService): updateGoalOfMatching 메소드 생성
    생성 및 수정

--- withmate/src/main/java/com/gdscewha/withmate/domain/matching/service/MatchingService.java
+++ withmate/src/main/java/com/gdscewha/withmate/domain/matching/service/MatchingService.java
@@ -1,19 +1,23 @@
 package com.gdscewha.withmate.domain.matching.service;
 
 import com.gdscewha.withmate.common.validation.ValidationService;
+import com.gdscewha.withmate.domain.journey.entity.Journey;
 import com.gdscewha.withmate.domain.matching.dto.MatchingDTO;
 import com.gdscewha.withmate.domain.matching.entity.Matching;
 import com.gdscewha.withmate.domain.matching.repository.MatchingRepository;
 import com.gdscewha.withmate.domain.member.entity.Member;
 import com.gdscewha.withmate.domain.model.Category;
+import com.gdscewha.withmate.domain.sticker.entity.Sticker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class MatchingService {
     private final MatchingRepository matchingRepository;
     private final ValidationService validationService;
-
 
     //매칭 객체 새로 만들기
     public Matching createMatching(Member member, MatchingDTO matchingDTO) {
@@ -27,9 +31,16 @@ public class MatchingService {
         return matchingRepository.save(matching);
     }
 
+    // 목표 입력 시 matching에 goal 저장하기 (matchingId로 goal을 찾아서 저장)
+    public Matching updateGoalOfMatching(MatchingDTO matchingDTO){
+        Matching matching = validationService.valMatching(matchingDTO.getId());
+        // 찾은 Matching 객체에 새로운 Goal 설정
+        matching.setGoal(matchingDTO.getGoal());
+        matchingRepository.save(matching);
 
-    // 목표 입력 시 저장하기
-    // matchingrepository에서 matchingid로 goal 찾아서 거기에 저장 save
+        return matching;
+    }
+
 
 
     // 카테고리 선택 시 matching의 category에 저장하기

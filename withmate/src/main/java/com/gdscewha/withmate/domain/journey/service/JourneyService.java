2703d41ffd54909170866faf38ccc6a10cd99636
seohyun-lee <applemint2003@gmail.com>
2024-01-25 14:37:52 +0900

    feat(Week, Journey) : 주와 여정 기능 개발

--- withmate/src/main/java/com/gdscewha/withmate/domain/journey/service/JourneyService.java
+++ withmate/src/main/java/com/gdscewha/withmate/domain/journey/service/JourneyService.java
@@ -5,6 +5,8 @@ import com.gdscewha.withmate.common.response.exception.JourneyException;
 import com.gdscewha.withmate.domain.journey.entity.Journey;
 import com.gdscewha.withmate.domain.journey.repository.JourneyRepository;
 import com.gdscewha.withmate.domain.relation.entity.Relation;
+import com.gdscewha.withmate.domain.week.entity.Week;
+import com.gdscewha.withmate.domain.week.service.WeekService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,23 +17,27 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class JourneyService {
     private final JourneyRepository journeyRepository;
+    private final WeekService weekService;
 
-    // 새로운 여정 생성: Relation을 받아서
-    public void createJourney(Relation relation) {
+    // 새로운 Journey 생성 및 저장: Relation을 받아서
+    public Journey createJourney(Relation relation) {
         List<Journey> existingJourneyList = journeyRepository.findAllByRelation(relation);
-        Integer journeyCount = existingJourneyList.size();
+        Integer journeyNum = existingJourneyList.size();
         Journey journey = Journey.builder()
-                .journeyNum(journeyCount.longValue()) // 처음에 0
-                .weekCount(1L) // 처음에 1L
+                .journeyNum(journeyNum.longValue()) // 처음에 1L
+                .weekCount(0L) // 처음에 0L
                 .relation(relation)
                 .build();
-        journeyRepository.save(journey);
+        return journeyRepository.save(journey);
     }
 
-    // 여정의 주 수 업데이트
-    public void updateWeekCountOfJourney(Journey journey){
+    // 현재 Journey의 Week 신규 생성
+    public Journey createNewWeekOfCurrentJourney(Relation relation){
+        Journey journey = getCurrentJourney(relation);
+        Week week = weekService.createWeek(journey);
         Long newWeekCount = journey.getWeekCount() + 1;
         journey.setWeekCount(newWeekCount);
+        return journeyRepository.save(journey);
     }
 
     // 단일 Journey 조회: Relation과 index로
@@ -42,14 +48,13 @@ public class JourneyService {
         throw new JourneyException(ErrorCode.JOURNEY_NOT_FOUND);
     }
 
-    // 현재 Journey를 반환 -> DTO로 변경 필요
+    // 현재 Journey를 조회 -> DTO로 변경 필요
     public Journey getCurrentJourney(Relation relation) {
         List<Journey> existingJourneyList = journeyRepository.findAllByRelation(relation);
         if (existingJourneyList == null || existingJourneyList.isEmpty())
             return null;
         Integer index = existingJourneyList.size() - 1;
-        Journey journey = existingJourneyList.get(index);
-        return journey;
+        return existingJourneyList.get(index);
     }
     
     // TODO: Relation에서 Journey 호출

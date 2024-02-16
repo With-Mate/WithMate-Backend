1241f7ef4216aaefe55cb0c5305de9d35e3b2a79
seohyun-lee <applemint2003@gmail.com>
2024-02-16 23:25:57 +0900

    Fix(sticker) : 스티커 조회 및 업데이트 에러 해결 및 코드 리팩토링

--- withmate/src/main/java/com/gdscewha/withmate/domain/sticker/dto/StickerDetailResDto.java
+++ withmate/src/main/java/com/gdscewha/withmate/domain/sticker/dto/StickerDetailResDto.java
@@ -1,6 +1,10 @@
 package com.gdscewha.withmate.domain.sticker.dto;
 
+import com.gdscewha.withmate.domain.sticker.entity.Sticker;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 @Getter
 @Setter
@@ -18,4 +22,21 @@ public class StickerDetailResDto {
     // 스티커 색상, 모양
     private String stickerColor;
     private String stickerShape;
+
+    public static StickerDetailResDto toDto(Sticker sticker, Boolean isMine){
+        String impressionTime = Optional.ofNullable(sticker.getImpressionTime())
+                .map(Object::toString)
+                .orElse("");
+        return StickerDetailResDto.builder()
+                .id(sticker.getId())
+                .title(sticker.getTitle())
+                .content(sticker.getContent())
+                .creationTime(sticker.getCreationTime().toString())
+                .impression(sticker.getImpression())
+                .impressionTime(impressionTime)
+                .isMine(isMine)
+                .stickerColor(sticker.getStickerColor())
+                .stickerShape(sticker.getStickerShape())
+                .build();
+    }
 }
